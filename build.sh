539e46d4392bb7639411969ea9e6c1b414ed8e8e
Auston Stewart <auston@shedbuilt.net>
2018-04-24 13:33:56 -1000

    Initial commit of sqlite3

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./configure --prefix=/usr \
+            --disable-static \
+            --enable-fts5 \
+            CFLAGS="-g -O2 \
+            -DSQLITE_ENABLE_FTS4=1 \
+            -DSQLITE_ENABLE_COLUMN_METADATA=1 \
+            -DSQLITE_ENABLE_UNLOCK_NOTIFY=1 \
+            -DSQLITE_ENABLE_DBSTAT_VTAB=1 \
+            -DSQLITE_SECURE_DELETE=1 \
+            -DSQLITE_ENABLE_FTS3_TOKENIZER=1" &&
+make -j $SHED_NUMJOBS &&
+make DESTDIR="$SHED_FAKEROOT" install
